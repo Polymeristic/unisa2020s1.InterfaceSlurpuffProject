e5ada047c9f6738933f02000f9c2a959e2531848
Oliver Mitchell <dev@polymeristic.com.au>
2020-06-13 01:31:50 +0930

    Added scene check

--- src/application/Controllers/GenerateReport.java
+++ src/application/Controllers/GenerateReport.java
@@ -35,6 +35,9 @@ public class GenerateReport extends AppController {
     /** Keep it in a static variable **/
     private static GenerateReport instance;
 
+    /** Scene associated with the instance **/
+    private Scene scene = null;
+
     /**
      * Use the LoadInstance method to load a new object and preserve that state for the rest of runtime
      */
@@ -48,6 +51,9 @@ public class GenerateReport extends AppController {
 
     @Override
     protected Scene loadAction() {
+        // Check if this instances scene has already been created
+        if (scene != null) return scene;
+
         //Borderpane Setup
         BorderPane root = new BorderPane();
 
@@ -331,7 +337,7 @@ public class GenerateReport extends AppController {
         bcYearly.getData().addAll(Year2015, Year2016, Year2017, Year2018, Year2019, Year2020);
 
         //Scene Setup + Show Stage
-        Scene scene = new Scene(root);
+        scene = new Scene(root);
 
         //Change to DaysFilter
         EventHandler<ActionEvent> backToDaysFilter = new EventHandler<>() {
